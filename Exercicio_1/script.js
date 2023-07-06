21c3c9926709b3704b0677898a596d90af4d8d00
anacirqueira <cirqueiraana6@gmail.com>
2023-07-06 16:59:28 -0300

    First commit

---
+++ Exercicio_1/script.js
@@ -0,0 +1,18 @@
+let number1 = Number(prompt("Digite o primeiro número: "))
+let number2 = Number(prompt("Digite o segundo número: "))
+
+const soma = number1 + number2
+
+const sub = number1 - number2
+
+const multi = number1 * number2
+
+const divisao = number1 / number2
+
+const resto = number1 % number2
+
+alert("A soma é igual a: " + soma)
+alert("A subtração é igual a: " + sub)
+alert("A multiplicação é igual a: " + multi)
+alert("A divisão é igual a: " + divisao)
+alert("O resto da divisão é igual a: " + resto)
